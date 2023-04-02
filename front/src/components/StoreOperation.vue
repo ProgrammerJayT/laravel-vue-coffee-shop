70a87106e37560662231ca2cd3d7b5d972fa6b9f
ProgrammerJayT <theanthem8@gmail.com>
2023-04-03 00:09:18 +0200

    Accepts props. store operations

--- front/src/components/StoreOperation.vue
+++ front/src/components/StoreOperation.vue
@@ -1,11 +1,18 @@
 <template>
     <div>
-
+        <ul class="list-unstyled text-start mx-auto list-hours mb-5">
+            <li class="d-flex list-unstyled-item list-hours-item" v-for="operationDay in operation">
+                {{ operationDay.day }}
+                <span class="ms-auto">{{ operationDay.hours == null ? 'Closed' : operationDay.hours }}</span>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
-
+    props: {
+        operation: Object
+    }
 }
 </script>
