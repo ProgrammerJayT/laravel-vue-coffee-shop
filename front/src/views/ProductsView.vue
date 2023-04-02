ebdea177d1101d1fcf69d5c9290a419fab54cfbb
ProgrammerJayT <theanthem8@gmail.com>
2023-04-02 22:34:37 +0200

    Imported navbar

--- front/src/views/ProductsView.vue
+++ front/src/views/ProductsView.vue
@@ -1,20 +1,6 @@
 <template>
-  <h1 class="text-center text-white d-none d-lg-block site-heading"><span class="text-primary site-heading-upper mb-3">A
-      Free Bootstrap 5 Business Theme&nbsp;</span><span class="site-heading-lower">Business Casual</span></h1>
-  <nav class="navbar navbar-dark navbar-expand-lg bg-dark py-lg-4" id="mainNav">
-    <div class="container"><a class="navbar-brand text-uppercase d-lg-none text-expanded" href="#">Brand</a><button
-        data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navbarResponsive"><span
-          class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav mx-auto">
-          <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
-          <li class="nav-item"><a class="nav-link" href="about.html">About us</a></li>
-          <li class="nav-item"><a class="nav-link" href="products.html">Products</a></li>
-          <li class="nav-item"><a class="nav-link" href="store.html">Store</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <NavBar />
+
   <section class="page-section">
     <div class="container">
       <div class="product-item">
@@ -76,3 +62,12 @@
     </div>
   </footer>
 </template>
+
+<script>
+import NavBar from '../components/NavBar.vue';
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
