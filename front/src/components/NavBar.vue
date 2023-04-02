adc6870c81202552bbe5e403db9855e7c852f311
ProgrammerJayT <theanthem8@gmail.com>
2023-04-02 22:54:26 +0200

    Added router link

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -9,13 +9,28 @@
                         class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav mx-auto">
-                        <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="about.html">About us</a></li>
-                        <li class="nav-item"><a class="nav-link" href="products.html">Products</a></li>
-                        <li class="nav-item"><a class="nav-link" href="store.html">Store</a></li>
+                        <li class="nav-item">
+                            <RouterLink to="/" class="nav-link">Home</RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink to="/about" class="nav-link">About us</RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink to="/products" class="nav-link">Products</RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink to="/store" class="nav-link">Store</RouterLink>
+                        </li>
                     </ul>
                 </div>
             </div>
         </nav>
     </div>
 </template>
+
+<script>
+import { RouterLink } from 'vue-router'
+export default {
+
+}
+</script>
