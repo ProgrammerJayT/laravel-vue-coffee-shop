aad65afca75dddea7a7ccd90b7ecfb71c76220a5
ProgrammerJayT <theanthem8@gmail.com>
2023-04-02 23:04:06 +0200

    Imported navbar and footer

--- front/src/App.vue
+++ front/src/App.vue
@@ -3,5 +3,20 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <NavBar />
+
   <RouterView />
+
+  <Footer />
 </template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
+export default {
+  components: {
+    NavBar,
+    Footer
+  }
+}
+</script>
